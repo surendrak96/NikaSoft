193e310c217707566fd4b4a05f0d3684cbd910fa
Surender Reddy <imsurendra99@gmail.com>
2016-10-23 22:09:57 +0530

    update send_mail.php

--- smtp/Send_Mail.php
+++ smtp/Send_Mail.php
@@ -2,17 +2,17 @@
 function Send_Mail($to,$subject,$body)
 {
 require 'class.phpmailer.php';
-$from       = "imsurendra99@gmail.com";
+$from       = "example@gmail.com";
 $mail       = new PHPMailer();
 $mail->IsSMTP(true);            // use SMTP
 $mail->IsHTML(true);
 $mail->SMTPAuth   = true;                  // enable SMTP authentication
 $mail->Host       = 'ssl://smtp.gmail.com'; // Amazon SES server, note "tls://" protocol
 $mail->Port       =  '465';                    // set the SMTP port
-$mail->Username   = "imsurendra99@gmail.com";  // SMTP  username
-$mail->Password   = "eswar2006";  // SMTP password
-$mail->SetFrom($from, 'imsurendra99@gmail.com');
-$mail->AddReplyTo($from,'imsurendra99@gmail.com');
+$mail->Username   = "example@gmail.com";  // SMTP  username
+$mail->Password   = "123456789";  // SMTP password
+$mail->SetFrom($from, 'example@gmail.com');
+$mail->AddReplyTo($from,'example@gmail.com');
 $mail->Subject    = $subject;
 $mail->MsgHTML($body);
 $address = $to;
